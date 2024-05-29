79c8a25e734b676947f074dd21cef84451a41c01
hunzz <hunzz.study@gmail.com>
2024-05-29 15:21:23 +0900

    PasswordConfig 클래스 생성하여 PasswordEncoder를 Bean 등록

---
+++ src/main/kotlin/hunzz/study/springauthjwt/global/config/PasswordConfig.kt
@@ -0,0 +1,11 @@
+package hunzz.study.springauthjwt.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Configuration
+class PasswordConfig {
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}
